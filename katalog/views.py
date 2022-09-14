b43545ed8225ee8b61aebda4e0a1ba2591822b27
Nicholas Lexi <lexi.nicholas03@gmail.com>
2022-09-15 00:55:14 +0700

    commit

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
+def show_katalog(request):
+    data_katalog_item = CatalogItem.objects.all()
+    context = {
+        'list_item' : data_katalog_item,
+        'name' : "Nicholas Lexi",
+        'id' : "2106720960",
+    }
+    return render(request, "katalog.html", context)
 # TODO: Create your views here.
