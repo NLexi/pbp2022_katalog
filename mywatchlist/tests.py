6a05c0a779cb3696e1f71e19b012b3bd155c2f5c
Nicholas Lexi <lexi.nicholas03@gmail.com>
2022-09-21 20:18:38 +0700

    commit

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
+class BasicTest(TestCase):
+    def test_html(self):
+        response = self.client.get('mywatchlist/html/')
+        self.assertEqual(response.status_code, 200)
+    
